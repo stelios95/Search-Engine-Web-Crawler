8adfa2ed62d9fb0d0dfe1ac3e5b5f41639b0b268
Asterios Ntinos <ntinosasterios@gmail.com>
2021-01-05 20:40:40 +0200

    [FIX] change interval logs

--- routes.js
+++ routes.js
@@ -8,7 +8,9 @@ app.use(cors())
 configRoute.route('/changeInterval').post((req, res) => {
   try{
     crons.changeCronInterval(crons.jobs.fullCrawlJob, req.body.crawlFreq, 1)
+    console.log(`New full scan frequency: Every ${req.body.crawlFreq} hours`)
     crons.changeCronInterval(crons.jobs.refreshJob, req.body.updateFreq, 0)
+    console.log(`New refresh frequency: Every ${req.body.crawlFreq} hours`)
     res.status(200).send('OK')
   } catch(err){
     console.log('ERROR: '+ err)
