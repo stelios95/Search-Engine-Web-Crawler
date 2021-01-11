4c6040ec9a003595504935211239f2bdc300532b
Asterios Ntinos <ntinosasterios@gmail.com>
2021-01-11 21:30:19 +0200

    [test] changed batch size to 10

--- crawlerConstants.js
+++ crawlerConstants.js
@@ -1,6 +1,6 @@
 const os = require('os')
 const DATABASE_STRING = "mongodb+srv://dbAdmin:yei6fahl@cluster0-xy1h1.mongodb.net/test?retryWrites=true&w=majority"
-const MASS_INSERT_RECORDS_SIZE = 30
+const MASS_INSERT_RECORDS_SIZE = 10
 const TITLE_LIMIT = 150
 const NUMBER_OF_THREADS = os.cpus().length
 const FULL_SCAN = 'FULL_SCAN'
